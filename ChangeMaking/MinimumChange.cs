53efb2564f9d5811c21af8218f6c6c45722774f3
neverendingqs <mark.tse@neverendingqs.com>
2015-02-22 17:06:42 -0500

    Added constructor.

--- ChangeMaking/MinimumChange.cs
+++ ChangeMaking/MinimumChange.cs
@@ -7,5 +7,19 @@ using System.Threading.Tasks;
 namespace ChangeMaking {
 
     public class MinimumChange {
+
+        public ISet<int> Denominations { get; set; }
+
+        public MinimumChange( ISet<int> denominations ) {
+            if( denominations == null ) {
+                throw new ArgumentNullException();
+            }
+
+            if( denominations.Count <= 0 || ! denominations.Contains(1)) {
+                throw new ArgumentException();
+            }
+            
+            this.Denominations = denominations;
+        }
     }
 }
--- ChangeMakingTests/MinimumChangeTests.cs
+++ ChangeMakingTests/MinimumChangeTests.cs
@@ -10,5 +10,50 @@ namespace ChangeMakingTests {
 
     [TestFixture]
     public class MinimumChangeTests {
+
+        [Test]
+        public void MinimumChange_CreateObject_ReturnsObject() {
+            /* Arrange */
+            ISet<int> denominations = new HashSet<int> { 1, 3, 5 };
+
+            /* Act */
+            MinimumChange mc = new MinimumChange( denominations );
+
+            /* Assert */
+            Assert.IsNotNull( mc );
+            CollectionAssert.AreEqual(
+                denominations,
+                mc.Denominations
+            );
+        }
+
+        [Test]
+        public void MinimumChange_EmptyDenominations_ThrowsArgumentException() {
+            /* Arrange */
+            ISet<int> denominations = new HashSet<int> {};
+
+            /* Act, Assert */
+            Assert.Throws<ArgumentException>( () => new MinimumChange( denominations ) );
+        }
+
+        [Test]
+        public void MinimumChange_NullDenominations_ThrowsArgumentNullException() {
+            /* Arrange */
+            ISet<int> denominations = null;
+
+            /* Act, Assert */
+            Assert.Throws<ArgumentNullException>( () => new MinimumChange( denominations ) );
+        }
+
+        [TestCase( new int[] { 2 } )]
+        [TestCase( new int[] { 2, 5, 9 } )]
+        [TestCase( new int[] { 5, 9, 100 } )]
+        public void MinimumChange_NoOneDenomination_ThrowsArgumentException( int[] invalidDenominations ) {
+            /* Arrange */
+            ISet<int> denominations = new HashSet<int>( invalidDenominations );
+
+            /* Act, Assert */
+            Assert.Throws<ArgumentException>( () => new MinimumChange( denominations ) );
+        }
     }
 }
